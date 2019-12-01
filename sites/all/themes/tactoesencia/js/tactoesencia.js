c06ff2439136e67b9551616277a1e34196a7ed17
rsayago <rafasayagoloz@gmail.com>
2019-12-01 18:03:26 +0100

    styles and languages selector

--- sites/all/themes/tactoesencia/js/tactoesencia.js
+++ sites/all/themes/tactoesencia/js/tactoesencia.js
@@ -1,14 +1,24 @@
 (function ($) {
     $(document).ready(function(){
 		//$('div.view-content').slick();
-		$('.slickPortada > .view-content').not('.slick-initialized').slick({
-			autoplay:true,
-            arrows: true,
-            prevArrow:"<button type='button' class='glyphicon glyphicon-chevron-left'></button>",
-            nextArrow:"<button type='button' class='glyphicon glyphicon-chevron-right'></button>"
+		$('.view-slider-front-multilenguage > .view-content').not('.slick-initialized').slick({
+			infinite: true,
+			dots: true,
+			arrows: false,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			fade: true,
+			fadeSpeed: 1000
+           // arrows: true,
+           // prevArrow:"<button type='button' class='glyphicon glyphicon-chevron-left'></button>",
+           // nextArrow:"<button type='button' class='glyphicon glyphicon-chevron-right'></button>"
 		});
+	$('.view-masajes-multilanguage > .view-content >.views-row').each(function(index) {
+		console.log(this.className);
+		
+		$(this).find('.msj-allinfo').wrapAll('<div class="toginfo" />');
+	});
 	
-
 
 
 	});
